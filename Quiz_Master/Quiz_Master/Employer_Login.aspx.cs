b7ed8216781339f048cef54b0297cfe7d9ce0d07
Anish <sharm19h@uwindsor.ca>
2020-11-23 23:22:18 -0500

    Intial Commit

---
+++ Quiz_Master/Quiz_Master/Employer_Login.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Quiz_Master
+{
+    public partial class Employer_Login : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        // Sign In button is clicked
+        protected void signin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if(con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("Select * from Employer where Employer_Name ='"+user_name.Text.Trim()+"' AND Employer_Password='"+password.Text.Trim()+"'", con);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                if(dr.HasRows)
+                {
+                    while(dr.Read())
+                    {
+                        Response.Write("<script>alert('" + dr.GetValue(1).ToString() + "');</script>");
+                    }
+
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid Credentials')</script>");
+                }
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        protected void user_name_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
